7c2575cab07e36621e2c6f2d290ca4e2fc621311
Andrea Bernini <andreabe99@gmail.com>
2023-05-30 11:15:57 +0200

    INIT: initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for the Multimodal Interaction course, for the master's degree program at the Sapienza University of Rome.',
+    author='AndreaBe99',
+    license='',
+)
