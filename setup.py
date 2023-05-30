c00880e3f8d91c07023947bafa090ab309e9af69
Andrea Bernini <andreabe99@gmail.com>
2023-05-30 17:42:29 +0200

    FIX: Modify string apex

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Project for the Multimodal Interaction course, for the master's degree program at the Sapienza University of Rome.',
+    description="Project for the Multimodal Interaction course, for the master's degree program at the Sapienza University of Rome.",
     author='AndreaBe99',
     license='',
 )
