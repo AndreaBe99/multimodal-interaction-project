9d4c02253e26617e234cddbfb3baf1152a3a1b01
Andrea Bernini <andreabe99@gmail.com>
2023-06-02 19:07:47 +0200

    FEAT: add a Enum class for OpenCV colors.

---
+++ src/libs/utils/colors.py
@@ -0,0 +1,9 @@
+import enum
+
+class Colors(enum.Enum):
+    RED = (0, 0, 255)  # BGR
+    GREEN = (0, 255, 0)  # BGR
+    BLUE = (255, 0, 0)  # BGR
+    YELLOW = (0, 255, 255)  # BGR
+    WHITE = (255, 255, 255)  # BGR
+    BLACK = (0, 0, 0)  # BGR
