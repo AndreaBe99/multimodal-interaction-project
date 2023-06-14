03d19697b125b21db57dfdadb0c91467cf5d6896
Andrea Bernini <andreabe99@gmail.com>
2023-06-14 15:06:23 +0200

    FEAT: move Colors class in this file.

--- src/libs/utils/colors.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Colors(Enum):
-    RED = (0, 0, 255)  # BGR
-    GREEN = (0, 255, 0)  # BGR
-    BLUE = (255, 0, 0)  # BGR
-    YELLOW = (0, 255, 255)  # BGR
-    WHITE = (255, 255, 255)  # BGR
-    BLACK = (0, 0, 0)  # BGR
--- src/libs/utils/config.py
+++ src/libs/utils/config.py
@@ -1,6 +1,22 @@
 from enum import Enum
 
+
+class Colors(Enum):
+    """
+    Colors used in the GUI
+    """
+    RED = (0, 0, 255)  # BGR
+    GREEN = (0, 255, 0)  # BGR
+    BLUE = (255, 0, 0)  # BGR
+    YELLOW = (0, 255, 255)  # BGR
+    WHITE = (255, 255, 255)  # BGR
+    BLACK = (0, 0, 0)  # BGR
+    
+    
 class Path(Enum):
+    """
+    Paths and Strings used in the GUI
+    """
     IMAGE_GUI_MAIN = "src/data/images/static/driver.png"
     IMAGE_GUI_VIDEO = "src/data/images/static/video-conference.png"
     IMAGE_GUI_AUDIO = "src/data/images/static/microphone.png"
