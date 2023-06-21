a2b7b0d0ca947e9746925965702bc3beb6075b18
Andrea Bernini <andreabe99@gmail.com>
2023-06-21 16:55:27 +0200

    FEAT: set the alarm if we pass more than 5 sec.

--- src/app/detection/distracted.py
+++ src/app/detection/distracted.py
@@ -1,19 +1,30 @@
 import sys
 import torch
 import pytorch_lightning as pl
+import time
 
 sys.path.append("./")
 from src.train.model import LitEfficientNet
 from src.train.config import StaticDataset as sd
+from src.app.utils.config import Colors
 
 class Distracted():
     
-    def __init__(self, path=sd.MODEL_PATH.value) -> None:
+    def __init__(self, time_treshold=5.0, path=sd.MODEL_PATH.value) -> None:
+        self.time_treshold = time_treshold
+        self.state = {
+            "start_time": time.perf_counter(),
+            "distracted_time": 0.0,  # Holds the amount of time passed with EAR < EAR_THRESH
+            "color": Colors.GREEN.value,
+            "play_alarm": False,
+            "class": 'c0',
+        }
+        
         if torch.cuda.is_available():
             self.device = torch.device('cuda')
         else:
             self.device = torch.device('cpu')
-            
+        
         self.model = LitEfficientNet.load_from_checkpoint(
             path, 
             map_location=self.device
@@ -29,5 +40,23 @@ class Distracted():
         # get index of the class
         index = pred.item()
         char_index = 'c' + str(index)
+
+        # if the class is not 'c0' (i.e. different from 'safe driving')
+        # then the driver is distracted, so we update the state and we check
+        # the time passed since the driver is distracted, if it is greater than
+        # the treshold, we play the alarm
+        if char_index != 'c0':
+            end_time = time.perf_counter()
+            self.state["distracted_time"] += end_time - self.state["start_time"]
+            self.state["start_time"] = end_time
+            self.state["color"] = Colors.RED.value
+            if self.state["distracted_time"] > self.time_treshold:
+                self.state["play_alarm"] = True
+        else:
+            self.state["start_time"] = time.perf_counter()
+            self.state["distracted_time"] = 0.0
+            self.state["color"] = Colors.GREEN.value
+            self.state["play_alarm"] = False
         
-        return char_index
+        self.state["class"] = char_index
+        return self.state
